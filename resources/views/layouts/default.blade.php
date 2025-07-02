e1a100f2b2429e9646bb7aab27cc377db57cc5d1
AssoDePicche <samuelprado730@gmail.com>
2025-07-02 18:24:38 -0300

    refactor: error display

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -31,16 +31,6 @@
             </navbar>
         @endauth
 
-        @if ($errors->any())
-            <div style="color: red;">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-
         @if (session('success'))
             <p style="color: green;">{{ session('success') }}</p>
         @endif
