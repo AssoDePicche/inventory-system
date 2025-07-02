58fe76078d2244144a32af430824ca29bc26228f
AssoDePicche <samuelprado730@gmail.com>
2025-07-02 15:30:07 -0300

    feat: routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\UserController;
 
 use Illuminate\Support\Facades\Route;
 
@@ -8,4 +11,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::resource('categories', CategoryController::class);
+
 Route::resource('products', ProductController::class);
+
+Route::resource('transactions', TransactionController::class);
+
+Route::resource('users', UserController::class);
