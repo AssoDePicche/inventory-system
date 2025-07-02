57e322bb1faf0fd054170e281c1355cc0a8841e4
AssoDePicche <samuelprado730@gmail.com>
2025-07-02 17:32:32 -0300

    feat: default layout

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang='{{ str_replace('_', '-', app()->getLocale()) }}'>
+    <head>
+        <meta charset='utf-8'>
+        <meta name='viewport' content='width=device-width,initial-scale=1'>
+        <title>{{ config('app.name') }}</title>
+    </head>
+
+    <body>
+        @auth
+            <navbar>
+                <div>{{ config('app.name') }}</div>
+
+                <ul>
+                    <li>
+                        <a href="{{ route('categories.create') }}">
+                            Categorias
+                        </a>
+                    </li>
+                    <li>
+                        <a href="{{ route('products.index') }}">
+                            Produtos
+                        </a>
+                    </li>
+                    <li>
+                        <a href="{{ route('transactions.index') }}">
+                            Transações
+                        </a>
+                    </li>
+                </ul>
+            </navbar>
+        @endauth
+
+        @if ($errors->any())
+            <div style="color: red;">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        @if (session('success'))
+            <p style="color: green;">{{ session('success') }}</p>
+        @endif
+
+        <main>
+            @yield('content')
+        </main>
+
+        <footer>
+            {{ config('app.name') }} &copy; {{ date('Y') }}
+        </footer>
+    </body>
+</html>
