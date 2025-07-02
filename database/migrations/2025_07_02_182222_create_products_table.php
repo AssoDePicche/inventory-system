3902238db4f705de63be1a609e65340251968d77
AssoDePicche <samuelprado730@gmail.com>
2025-07-02 15:25:45 -0300

    fix: migrations

--- database/migrations/2025_07_02_182222_create_products_table.php
+++ database/migrations/2025_07_02_182222_create_products_table.php
@@ -10,11 +10,12 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('sku')->unique();
+            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
+            $table->string('name')->unique();
             $table->text('description')->nullable();
             $table->decimal('price', 10, 2);
             $table->integer('quantity');
+            $table->integer('min_quantity')->default(1);
             $table->timestamps();
         });
     }
